4cdb9c1e5fff859aa066ce778d49470e64b0561c
Jon M <jsm22601@gmail.com>
2024-05-14 18:41:39 -0400

    Added Userlogin backend endopoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,9 +3,18 @@ const { restoreUser } = require('../../utils/auth.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
