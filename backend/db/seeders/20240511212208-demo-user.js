468670fba8b400ec34ef33afe1348b25b957cc65
Jon M <jsm22601@gmail.com>
2024-05-16 18:51:34 -0400

    added firstName lastName to users and updated routes to reflect

--- backend/db/seeders/20240511212208-demo-user.js
+++ backend/db/seeders/20240511212208-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Joe',
+        lastName: 'Schmoe',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Jon',
+        lastName: 'Doe',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Jane',
+        lastName: 'Doe',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
