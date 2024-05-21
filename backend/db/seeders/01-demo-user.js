1f1f36f49873b673aa815d1b086207316625d2eb
Jon M <jsm22601@gmail.com>
2024-05-21 18:48:32 -0400

    attempted deployment fix

--- backend/db/seeders/01-demo-user.js
+++ backend/db/seeders/01-demo-user.js
@@ -13,23 +13,23 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
-        firstName: 'Joe',
-        lastName: 'Schmoe',
-        email: 'demo@user.io',
+        firstName: 'Hulda',
+        lastName: 'Nord',
+        email: 'demo@tamriel.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
-        firstName: 'Jon',
-        lastName: 'Doe',
-        email: 'user1@user.io',
+        firstName: 'Keerava',
+        lastName: 'Argonian',
+        email: 'user1@tamriel.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
-        firstName: 'Jane',
-        lastName: 'Doe',
-        email: 'user2@user.io',
+        firstName: 'Brynjolf',
+        lastName: 'Nord',
+        email: 'user2@tamriel.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
