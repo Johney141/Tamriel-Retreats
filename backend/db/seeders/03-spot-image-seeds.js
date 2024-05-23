642d50a7067312aff2947984de598ee207edfd40
Jonathan Moesta <jsm22601@gmail.com>
2024-05-23 10:10:34 -0400

    added down for SpotImages seeder

--- backend/db/seeders/03-spot-image-seeds.js
+++ backend/db/seeders/03-spot-image-seeds.js
@@ -23,7 +23,7 @@ module.exports = {
         url: 'https://tamriel-retreats.onrender.com/images/The_Bee_and_Barb_preview.jpg',
         spotId: '2',
         isPreview: true
-      },q
+      },
       {
         url: 'https://tamriel-retreats.onrender.com/images/The_Ragged_Flagon_preview.jpg',
         spotId: '3',
@@ -44,10 +44,20 @@ module.exports = {
         spotId: '3',
         isPreview: false
       }
-    ])
+    ], {validate: true});
   },
 
   async down (queryInterface, Sequelize) {
-
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ['https://tamriel-retreats.onrender.com/images/The_Bannered_Mare_preview.jpg',
+                       'https://tamriel-retreats.onrender.com/images/The_Bee_and_Barb_preview.jpg',
+                       'https://tamriel-retreats.onrender.com/images/The_Ragged_Flagon_preview.jpg',
+                       'https://tamriel-retreats.onrender.com/images/The_Bannered_Mare.jpg',
+                       'https://tamriel-retreats.onrender.com/images/The_Bee_and_Barb.jpg',
+                       'https://tamriel-retreats.onrender.com/images/Ragged_Flagon.jpg',
+                      ] }
+    }, {});
   }
 };
