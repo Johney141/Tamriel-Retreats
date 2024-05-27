121f66b4fcaeea4ee0f443568f0b9f0fec70fa36
Jon M <jsm22601@gmail.com>
2024-05-27 19:57:03 -0400

    added routes to create and edit reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -54,6 +54,107 @@ router.get('/current', requireAuth, async (req, res, next) => {
     }
 })
 
+router.get('/:spotId', async (req, res, next) => {
+    try {
+        const spotId = parseInt(req.params.spotId);
+        const reviews = await Review.findAll({
+            where: { spotId },
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'firstName', 'lastName']
+                },
+                {
+                    model: ReviewImage,
+                    attributes: ['id', 'url']
+                }
+            ]
+        });
+    
+        if(!reviews) {
+            const noSpots = new Error("Spot couldn't be found")
+            noSpots.status = 404;
+            return next(noSpots);
+        }
+        res.json({
+            Reviews: reviews
+        })
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+router.post('/:spotId', requireAuth, async (req, res, next) => {
+    try {
+        const spotId = parseInt(req.params.spotId);
+        const userId = req.user.id
+        const spot = await Spot.findByPk(spotId);
+        const reviewCheck = await Review.findOne({
+            where: {
+                userId,
+                spotId,
+            }
+        })
+        if(!spot) {
+            const noSpot = new Error("Spot couldn't be found");
+            noSpot.status = 404;
+            return next(noSpot);
+        }
+        if(reviewCheck) {
+            const existingReview = new Error("User already has a review for this spot");
+            return next(existingReview);
+        }
+    
+        const { review, stars } = req.body;
+    
+    
+        const newReview = await Review.create({
+            userId,
+            spotId,
+            review,
+            stars
+        });
+    
+        res.json(newReview);
+    } catch (error) {
+       next(error) 
+    }
+})
+
+router.put('/:reviewId', requireAuth, async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        const reviewId = parseInt(req.params.reviewId);
+        const updateReview = await Review.findByPk(reviewId);
+
+        if(!updateReview) {
+            const noReview = new Error("Review couldn't be found");
+            noReview.status = 404;
+            return next(noReview);
+        };
+        if(updateReview.userId !== userId){
+          const notAuth = new Error("Forbidden");
+          notAuth.status = 403;
+          return next(notAuth);
+        };
+
+        const { review, stars } = req.body;
+        
+        await updateReview.update({
+            userId,
+            spotId: updateReview.spotId,
+            review,
+            stars
+        })
+
+        res.json(updateReview);
+
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 
 module.exports = router;
