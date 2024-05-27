943bc64b0714f1c00ea0df94c6ce8b40a3aff4b4
Jon M <jsm22601@gmail.com>
2024-05-27 17:24:40 -0400

    Added remaining spot routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,9 +2,10 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 
 const { restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot, Review, SpotImage } = require('../../db/models');
+const { Spot, Review, SpotImage, User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { fullSpots, fullSpot } = require('../../utils/spots')
 
 
 
@@ -12,68 +13,196 @@ const router = express.Router();
 
 router.use(restoreUser)
 
-const fullSpot = async (spots) => {
-    let updatedSpots = []
-    for(let spot of spots){
-        const spotId = spot.id;
-        const reviews = await Review.findAll({
-            where: {spotId}
-        });
-        let reviewCount = reviews.length;
-        let starSum = reviews.reduce((sum, review) => sum + review.stars, 0);
-
-        let updatedSpot = spot.toJSON();
-
-        updatedSpot.avgRating = starSum / reviewCount;
-        
-
-
-        const previewImage = await SpotImage.findOne({
-            where: {
-                spotId,
-                isPreview: true
-            }
-        })
-        
-        updatedSpot.previewImage = previewImage.url;
-        updatedSpots.push(updatedSpot)
-    }
-    return updatedSpots
-}
-
-
 router.get('/', async (req, res, next) => {
     try {
-        const spots = await Spot.findAll()
+        const spots = await Spot.findAll({
+            include: [
+                {
+                    model: Review,
+                    required: false
+                },
+                {
+                    model: SpotImage,
+                    where: {
+                        isPreview: true
+                    },
+                    required: false
+                }
+            ]
+        })
 
         if(!spots){
             next(new Error('No spots currently exist'))
         }
         
-        let updatedSpots = await fullSpot(spots)
+        let updatedSpots = await fullSpots(spots)
            
-        res.json(updatedSpots)
+        res.json({
+            Spots: updatedSpots
+        })
     } catch (error) {
         next(error)
     }
 })
 
 router.get('/current', requireAuth, async (req, res, next) => {
-    const userId = req.user.id;
-    const spots = await Spot.findAll({
-        where: {
-            ownerId: userId
+    try {
+        const userId = req.user.id;
+        const spots = await Spot.findAll({
+            where: {
+                ownerId: userId
+            },
+            include: [
+                {
+                    model: Review
+                },
+                {
+                    model: SpotImage,
+                    where: {
+                        isPreview: true
+                    }
+                }
+            ]
+        })
+        if(!spots) {
+            next(new Error('Current User does not have any spots'))
         }
-    })
-
-    const updatedSpots = await fullSpot(spots)
-
-    res.json({
-        Spots: updatedSpots
-    })
+    
+        const updatedSpots = await fullSpots(spots)
+    
+        res.json({
+            Spots: updatedSpots
+        })
+    } catch (error) {
+        next(error)
+    }
 
 })
 
+router.get('/:spotId', async (req, res, next) => {
+    try {
+        const spotId = parseInt(req.params.spotId);
+
+        const spot = await Spot.findByPk(spotId, {
+            include: [
+                {
+                    model: Review
+                },
+                {
+                    model: SpotImage,
+                    attributes: {
+                        exclude: ['createdAt', 'updatedAt', 'spotId']
+                    }
+                },
+                {
+                    model: User,
+                    as: 'Owner',
+                    attributes: ['id', 'firstName', 'lastName']
+                }
+            ]
+        })
+
+        if(!spot) {
+            next(new Error("Spot couldn't be found"))
+        }
+
+        const updatedSpot = fullSpot(spot);
+
+        res.json(updatedSpot)
+
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.post('/', requireAuth, async (req, res, next) => {
+    try {
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        const ownerId = req.user.id;
+        const newSpot = await Spot.create({
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price,
+            ownerId
+        })
+
+        res.json(newSpot);
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.put('/:spotId', requireAuth, async (req, res, next) => {
+    try {
+        const spotId = parseInt(req.params.spotId);
+        const userId = req.user.id;
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        const spot = await Spot.findByPk(spotId);
+        console.log(userId)
+        console.log(spot.ownerId)
+
+        if(!spot) {
+            next(new Error("Spot couldn't be found"))
+        }
+
+        if(spot.ownerId !== userId) {
+            next(new Error("Forbidden"))
+        }
+        
+
+        await spot.update({
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price,
+            ownerId: userId
+        })
+
+        res.json(spot);
+
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+router.delete('/:spotId', requireAuth, async (req, res, next) => {
+    try {
+        const spotId = parseInt(req.params.spotId);
+        const userId = req.user.id;
+        const spot = await Spot.findByPk(spotId)
+
+        if(!spot) {
+            next(new Error("Spot couldn't be found"))
+        }
+
+        if(spot.ownerId !== userId) {
+            next(new Error("Forbidden"))
+        }
+        
+        await spot.destroy();
+
+        res.json({
+            message: "Successfully deleted"
+          })
+
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 
