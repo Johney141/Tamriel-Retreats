0bac7fc62cc90bc5c25f0270c24750b576c43aee
Jon M <jsm22601@gmail.com>
2024-06-25 19:12:04 -0400

    Added csrf authtenication

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,14 @@ export default defineConfig(({ mode }) => ({
   ],
   // To automatically open the app in the browser whenever the server starts,
   // uncomment the following lines:
-  // server: {
-  //   open: true
-  // }
+  server: {
+    open: true
+  },
+  
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    },
+  }
+
 }));
