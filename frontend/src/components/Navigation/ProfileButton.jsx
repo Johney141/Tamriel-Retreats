10df6e97ba6aff8215b013a675f556848c0c75fd
Jon M <jsm22601@gmail.com>
2024-06-26 21:08:51 -0400

    added profile dropdown

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,12 +1,51 @@
-import { CgProfile } from "react-icons/cg";
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { FaUserCircle } from 'react-icons/fa';
+import * as sessionActions from '../../store/session';
+import './ProfileButton.css'
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+  const toggleMenu = (e) => {
+    e.stopPropagation();
+    setShowMenu(!showMenu);
+  };
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
 
 
-const ProfileButton = () => {
-    return (
-        <div style={{fontSize: "30px"}}>
-            <CgProfile />
-        </div>
-    )
-};
+  return (
+    <>
+      <button onClick={toggleMenu}>
+        <FaUserCircle />
+      </button>
+      <ul className={ulClassName}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
+}
 
-export default ProfileButton
+export default ProfileButton;
