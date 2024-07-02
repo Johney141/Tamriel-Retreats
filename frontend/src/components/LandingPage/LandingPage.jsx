144c64c4ae201f398b9d36928738502cde1b0300
Jonathan Moesta <jsm22601@gmail.com>
2024-07-02 15:28:24 -0400

    Updated linter and css

---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { useDispatch, useSelector } from "react-redux"
+
+
+const LandingPage = () => {
+    const dispatch = useDispatch();
+
+    const spots = useSelector()
+
+    return(
+        <h1>placeholder</h1>
+    )
+}
+
+export default LandingPage
