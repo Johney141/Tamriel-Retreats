ca40eb6ec44ecc27e3088a4c902218a6f20a80ef
Jon M <jsm22601@gmail.com>
2024-07-08 21:36:02 -0400

    end of day commit

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 
 const SpotDetails = () => {
     const [isLoaded, setIsLoaded] = useState(false);
     const { spotId } = useParams();
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        
+    })
 
     return (
         <h1>placeholder</h1>
